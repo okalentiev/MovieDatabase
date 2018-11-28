cb465f3b0ebd88e20fc51ba8def4bd095330bcea
Oleksii Kalentiev <akalentyev@gmail.com>
2018-11-28 19:33:24 +0100

    Initial setup of the XCode project

---
+++ MovieDatabase/Common/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MovieDatabase
+//
+//  Created by Oleksii Kalentiev on 11/28/18.
+//  Copyright © 2018 Oleksii Kalentiev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        let keyWindow = UIWindow(frame: UIScreen.main.bounds)
+
+        keyWindow.rootViewController = ViewController()
+        keyWindow.makeKeyAndVisible()
+
+        return true
+    }
+
+}
+
---
+++ MovieDatabase/Modules/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MovieDatabase
+//
+//  Created by Oleksii Kalentiev on 11/28/18.
+//  Copyright © 2018 Oleksii Kalentiev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
